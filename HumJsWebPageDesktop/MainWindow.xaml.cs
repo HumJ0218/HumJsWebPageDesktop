d830fba9686c55f8fd12a53b3394cfd17b0a948d
tlos1 <tlos1@LAPTOP-EH54MTMM>
2021-10-18 11:31:07 +0800

    添加项目文件。

---
+++ HumJsWebPageDesktop/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace HumJsWebPageDesktop
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public const string ScriptJs = "script.js";
+        public const string MessageDirectory = "./Message";
+
+        public MainWindow()
+        {
+            Directory.CreateDirectory(MessageDirectory);
+            InitializeComponent();
+        }
+
+        private void WebView_CoreWebView2InitializationCompleted(object sender, CoreWebView2InitializationCompletedEventArgs e)
+        {
+            webView.CoreWebView2.DocumentTitleChanged += CoreWebView2_DocumentTitleChanged;
+        }
+
+        private void CoreWebView2_DocumentTitleChanged(object sender, object e)
+        {
+            Title = webView.CoreWebView2.DocumentTitle;
+        }
+
+        /// <summary>
+        /// 设置状态栏文字
+        /// </summary>
+        public void SetStatus(string content)
+        {
+            StatusShadow1.Content = content;
+            StatusShadow2.Content = content;
+            StatusShadow3.Content = content;
+            StatusShadow4.Content = content;
+            StatusShadow5.Content = content;
+            StatusShadow6.Content = content;
+            StatusShadow7.Content = content;
+            StatusShadow8.Content = content;
+            Status.Content = content;
+        }
+
+        /// <summary>
+        /// WebView2 开始导航，导航结果为网络请求。在事件期间，主机可能会禁止该请求。
+        /// </summary>
+        private void WebView_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            SetStatus(nameof(WebView_NavigationStarting));
+
+            webView.Visibility = Visibility.Hidden;
+        }
+
+        /// <summary>
+        /// WebView2 的源将更改到新的 URL。该事件可能由不会导致网络请求（如片段导航）的导航操作导致。
+        /// </summary>
+        private void WebView_SourceChanged(object sender, CoreWebView2SourceChangedEventArgs e)
+        {
+            SetStatus(nameof(WebView_SourceChanged));
+        }
+
+        /// <summary>
+        /// WebView 开始加载新页面的内容。
+        /// </summary>
+        private void WebView_ContentLoading(object sender, CoreWebView2ContentLoadingEventArgs e)
+        {
+            SetStatus(nameof(WebView_ContentLoading));
+        }
+
+        /// <summary>
+        /// WebView2 完成新页面上的内容加载。
+        /// </summary>
+        private void WebView_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            SetStatus(nameof(WebView_NavigationCompleted));
+
+            if (File.Exists(ScriptJs))
+            {
+                webView.CoreWebView2.ExecuteScriptAsync(File.ReadAllText(ScriptJs));
+            }
+
+            webView.Visibility = Visibility.Visible;
+        }
+
+        private void webView_WebMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            var now = DateTime.Now;
+            var source = e.Source;
+            var json = e.WebMessageAsJson;
+            var message = e.TryGetWebMessageAsString();
+
+            File.AppendAllLines($"{MessageDirectory}{now:yyyyMMdd}.log", new[] {
+                now.ToString().PadRight(32, '-'),
+                source,
+                json,
+                message,
+                "".PadRight(32, '-')
+            });
+        }
+    }
+}
---
+++ HumJsWebPageDesktop/script.js
@@ -0,0 +1,87 @@
+﻿const uri = 'https://live.bilibili.com';
+const cover = true;
+
+if (window.location.href == 'about:blank') {
+    window.location.href = uri;
+} else {
+    var video;
+    var interval = setInterval(() => {
+        var v = document.querySelector('video');
+        if (v) {
+            video = v;
+            clearInterval(interval);
+            main();
+        }
+    });
+
+    function main() {
+        try {
+            video.muted = true;
+            document.body.innerHTML = '';
+            document.body.appendChild(video);
+
+            video.style.background = 'black';
+            video.style.position = 'absolute';
+            video.style.zIndex = 1000000;
+            document.body.style.overflow = 'hidden';
+
+            var oldWidth = 0;
+            var oldHeight = 0;
+
+            function resize() {
+                var windowWidth = window.innerWidth;
+                var windowHeight = window.innerHeight;
+                var windowRatio = windowWidth / windowHeight;
+
+                if (windowWidth != oldWidth || windowHeight != oldHeight) {
+                    oldWidth = windowWidth;
+                    oldHeight = windowHeight;
+
+                } else {
+                    return;
+                }
+
+                var videoWidth = video.videoWidth;
+                var videoHeight = video.videoHeight;
+                var videoRatio = videoWidth / videoHeight;
+
+                if (cover) {
+                    if (windowRatio > videoRatio) {
+                        var width = windowWidth;
+                        var height = windowWidth / videoRatio;
+                        var top = (windowHeight - height) / 2;
+
+                        video.style.left = '0';
+                        video.style.top = top + 'px';
+                        video.style.width = width + 'px';
+                        video.style.height = height + 'px';
+                    } else if (windowRatio < videoRatio) {
+                        var width = windowHeight * videoRatio;
+                        var height = windowHeight;
+                        var left = (windowWidth - width) / 2;
+
+                        video.style.left = left + 'px';
+                        video.style.top = '0';
+                        video.style.width = width + 'px';
+                        video.style.height = height + 'px';
+                    } else {
+                        video.style.left = '0';
+                        video.style.top = '0';
+                        video.style.width = windowWidth + 'px';
+                        video.style.height = windowHeight + 'px';
+                    }
+                } else {
+                    video.style.left = '0';
+                    video.style.top = '0';
+                    video.style.width = windowWidth + 'px';
+                    video.style.height = windowHeight + 'px';
+                }
+            };
+
+            window.addEventListener('resize', resize);
+            resize();
+        } catch (ex) {
+            alert(ex)
+        }
+    }
+}
